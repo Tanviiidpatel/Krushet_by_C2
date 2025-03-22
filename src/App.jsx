71c34a86a94ec74a744c1b0527646d7c44f08126
Krish patel <krishp2304@gmail.com>
2025-03-22 11:07:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-red-500'>
+        hello world
+      </div>
+    </>
+  )
+}
+
+export default App
