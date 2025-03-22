8a8e60ca0aed50aba3925f14e5dbde97c6179e58
frsmit <156654986+frsmit@users.noreply.github.com>
2025-03-22 14:15:33 +0530

    branch merging

--- server/server.js
+++ server/server.js
@@ -3,12 +3,9 @@ import connectDb from "./utils/db.js";
 import dotenv from "dotenv";
 import register from "./router/farmer-router.js";
 import login from "./router/farmer-router.js";
-<<<<<<< HEAD
+import cors from "cors";
 import c_register from "./router/consumer-router.js";
 import c_login from "./router/consumer-router.js";
-=======
-import cors from "cors";
->>>>>>> d934458266331e90fb1639aa8d92b230548ac0b7
 
 dotenv.config();
 const app = express();
