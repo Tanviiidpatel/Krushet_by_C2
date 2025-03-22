1572f983af56defb7de39a4186520839e5063c8e
frsmit <156654986+frsmit@users.noreply.github.com>
2025-03-22 18:38:24 +0530

    crop api is up now

--- server/router/crop-router.js
+++ server/router/crop-router.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { searchCrops } from "../controller/crop-controller.js";
+import { searchCrops, postCropListing } from "../controller/crop-controller.js";
 
 const router = express.Router();
 
 router.post("/search", searchCrops); // Use POST instead of GET
+router.post("/listingcrops",postCropListing);
 
 export default router;
